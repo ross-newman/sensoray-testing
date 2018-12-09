39a12633415d565a48e04fee9e5dd9b5d7456bb8
Ross Newman <ross.newman@abaco.com>
2018-12-09 13:59:39 +0000

    Fixed vlc playback and re-tested

--- capture-test.sh
+++ capture-test.sh
@@ -7,7 +7,7 @@ rm ./input.yuv
 gcc capture.c -lv4l2 -o capture
 ./capture -d /dev/video1 -f -o > input.yuv
 
-# cvlc --demux rawvideo --rawvid-fps 25 --rawvid-width 720 --rawvid-height 576 --rawvid-chroma I420 input.yuv  
+# cvlc --demux rawvideo --rawvid-fps 25 --rawvid-width $WIDTH --rawvid-height $HEIGHT --rawvid-chroma UYVY input.yuv  
 
 gst-launch-1.0 filesrc location=./input.yuv ! videoparse height=$HEIGHT width=$WIDTH format=uyvy framerate=1/25 ! queue ! videoconvert ! xvimagesink sync=false
 
